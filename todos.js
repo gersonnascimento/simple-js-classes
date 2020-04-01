ea7a759556a17cbfa57c146d2e559b74e5b40aa8
Gerson Nascimento <gerson.nascimento@locaweb.com.br>
2020-04-01 20:03:17 -0300

    Create remove todo function

--- todos.js
+++ todos.js
@@ -9,13 +9,41 @@ var todos = [
 ];
 
 function renderTodos(){
+
+  listElement.innerHTML = '';
   for (todo of todos){
     var todoElement = document.createElement('li');
     var todoText = document.createTextNode(todo);
 
+    var linkElement = document.createElement('a');
+    var linkText = document.createTextNode('Excluir');
+
+    linkElement.setAttribute('href', '#');
+
+    var position = todos.indexOf(todo);
+    linkElement.setAttribute('onclick', 'deleteTodo('+position+')');
+    linkElement.appendChild(linkText);
+
     todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
+
     listElement.appendChild(todoElement);
   }
 }
 
 renderTodos();
+
+function addTodo(){
+  var todoText = inputElement.value;
+
+  todos.push(todoText);
+  inputElement.value = '';
+  renderTodos();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(position){
+  todos.splice(position, 1);
+  renderTodos();
+}
